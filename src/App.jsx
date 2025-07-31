320b09afc350a80d7353774fa95543a5419cbf50
Oreoluwa Alaba <alabaoreoluwa@gmail.com>
2025-07-31 21:34:13 +0100

    Font fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React, { Suspense, lazy } from 'react'
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProjectInfo from './pages/ProjectInfo';
+import ProjectInfo from "./pages/ProjectInfo";
 
-
-const Home = lazy(() => import('./pages/Home'))
-const JoinUs = lazy(() => import('./pages/JoinUs'))
-const JobApplication = lazy(() => import('./pages/JobApplication'))
-const OurWork = lazy(() => import('./pages/OurWork'))
+const Home = lazy(() => import("./pages/Home"));
+const JoinUs = lazy(() => import("./pages/JoinUs"));
+const JobApplication = lazy(() => import("./pages/JobApplication"));
+const OurWork = lazy(() => import("./pages/OurWork"));
 // const AppLayout = lazy(() => import('./components/appLayout/AppLayout'))
 // const Account = lazy(() => import('./pages/accounts/Account'))
 
-
-
 const App = () => {
   return (
-    <main className="font-sans">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/joinUs" element={<JoinUs />} />
-        <Route path="/ourWork" element={<OurWork />} />
-        <Route path="/ourWork/:id" element={<ProjectInfo />} />
-        <Route path="/joinUs/:id" element={<JobApplication />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/joinUs" element={<JoinUs />} />
+          <Route path="/ourWork" element={<OurWork />} />
+          <Route path="/ourWork/:id" element={<ProjectInfo />} />
+          <Route path="/joinUs/:id" element={<JobApplication />} />
+        </Routes>
+      </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
